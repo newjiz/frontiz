5168b540b8f7ab6118d0633c53fa1412fe667d40
2pac <tarasyarema@pm.me>
2019-11-04 01:41:31 +0100

    Frontend pseudofuncional, fer sessio ja tindirem MVP

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import axios from 'axios';
 import ReactDOM from 'react-dom';
 import {browserHistory} from 'react-router-dom';
 import {BrowserRouter as Router, Route, Redirect, Link} from 'react-router-dom'
@@ -10,6 +9,7 @@ import {MenuIcon} from '@material-ui/icons/Menu';
 
 import 'typeface-roboto';
 import './index.css';
+import { getThemeProps } from '@material-ui/styles';
 
 
 const useStyles = makeStyles(theme => ({
@@ -46,6 +46,8 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const URL = "http://0.0.0.0:5000";
+let TOKEN = undefined;
+// const TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoiNWRiZjNjMTdmMTJjYWNhZmU0NGU0YTI0In0.NsDTZGPT-kqzWu8wNkUBHeJZ-CtyG16tmf9NbqPMCNUV1eLUa1M89gkSyYa5S0e3U6zgLfAri9RFs8cyISVcHw";
 
 const Nav = () => {
     const classes = useStyles();
@@ -57,10 +59,6 @@ const Nav = () => {
 
     const handleClose = () => {
         setAnchorEl(null);
-    };
-
-    const handleRedirect = (url) => event => {
-        console.log(url);
     };
 
     return (
@@ -82,8 +80,11 @@ const Nav = () => {
                     >
                         <MenuItem><Button component={Link} to={"/"}>home</Button></MenuItem>
                         <MenuItem><Button component={Link} to={"/login"}>login</Button></MenuItem>
-                        <MenuItem><Button component={Link} to={"/u/1"}>profile</Button></MenuItem>
+                        <MenuItem><Button component={Link} to={"/register"}>register</Button></MenuItem>
+                        <MenuItem><Button component={Link} to={"/submit"}>submit</Button></MenuItem>
+                        <MenuItem><Button component={Link} to={"/user"}>profile</Button></MenuItem>
                         <MenuItem><Button component={Link} to={"/ranking"}>ranking</Button></MenuItem>
+                        <MenuItem><Button component={Link} to={"/ranking2"}>ranking2</Button></MenuItem>
                     </Menu>
                 </Toolbar>
             </AppBar>
@@ -91,9 +92,8 @@ const Nav = () => {
     );
 };
 
-const User = ({match}) => {
+const User = () => {
     const [user, setUser] = useState([]);
-    const id = match.params.id;
     const classes = useStyles();
 
     useEffect(() => {
@@ -101,14 +101,23 @@ const User = ({match}) => {
     }, []);
 
     async function getUser() {
-        const r = await fetch(URL + "/u/" + id);
-        const user = await r.json();
-        const r2 = await fetch(URL + "/u/" + id + "/c");
-        const content = await r2.json();
-        
+        const headers = new Headers({
+            "Authorization": TOKEN
+        });
+
+        const init = {
+          method: "GET",
+          headers: headers,
+          mode: "cors",
+        };
+
+        const request = new Request(URL + "/", init);
+        const r = await fetch(request);
+        const r_data = await r.json();
+
         let user_data = {
-            user: user.data,
-            content: content.data
+            user: r_data.data.user,
+            content: r_data.data.content
         };
 
         setUser(user_data);
@@ -123,7 +132,8 @@ const User = ({match}) => {
             {user.content && user.content.map(c => (
                 <Paper className={classes.root}>
                 <Typography component="p">
-                "{c.content}" - {new Date(c.created * 1000).toDateString()}, <i>{user.user.username}</i>
+                "{c.content.data}"
+                - <i>{new Date(c.created.$date).toDateString()}, {user.user.username}</i>
                 </Typography>
                 </Paper>
             ))}
@@ -140,7 +150,7 @@ const Ranking = () => {
     }, []);
 
     async function getRanking() {
-        const r = await fetch(URL + "/r");
+        const r = await fetch(URL + "/ranking");
         const ranking = await r.json();
         setRanking(ranking.data);
     }
@@ -149,16 +159,57 @@ const Ranking = () => {
     return ([
         <Nav />,
         <Container maxwidth="sm">
-        <Typography variant="h2" component="h2">Ranking</Typography>
+        <Typography variant="h2" component="h2">JizLO &copy;</Typography>
         {ranking.map(r => (
             <div>
             <Paper className={classes.root}>
             <Typography variant="h5" component="h3">
-            {r.score}
+            <b>{r.votes.elo.toFixed(2)}</b>
+            </Typography>
+            <Typography variant="h5" component="h2">
+            "{r.content.data}"
             </Typography>
             <Typography component="p">
-            "{r.content}"
-            - <i>{r.username}</i>
+            by <i>{r.user_id.$oid}</i>
+            </Typography>
+            </Paper>
+            <br></br>
+            </div>
+        ))}
+        </Container>
+    ]);
+};
+
+const Ranking2 = () => {
+    const [ranking, setRanking] = useState([]);
+    const classes = useStyles();
+
+    useEffect(() => {
+        getRanking();
+    }, []);
+
+    async function getRanking() {
+        const r = await fetch(URL + "/ranking2");
+        const ranking = await r.json();
+        setRanking(ranking.data);
+    }
+
+
+    return ([
+        <Nav />,
+        <Container maxwidth="sm">
+        <Typography variant="h2" component="h2">Votiz &copy;</Typography>
+        {ranking.map(r => (
+            <div>
+            <Paper className={classes.root}>
+            <Typography variant="h5" component="h3">
+            <b>{r.score_p.toFixed(2)} ({r.score_v})</b> 
+            </Typography>
+            <Typography variant="h5" component="h2">
+            "{r.content.data}"
+            </Typography>
+            <Typography component="p">
+            by <i>{r.user_id.$oid}</i>
             </Typography>
             </Paper>
             <br></br>
@@ -179,16 +230,35 @@ const Login = () => {
         setValues({ ...values, [name]: event.target.value });
     };
 
-    const handleSubmit = () => event => {
-        console.log(values);
+    async function handleSubmit(event) {
         event.preventDefault();
+
+        const data = {
+            "username": values.username,
+            "password": values.password
+        };
+
+        const headers = {"Content-Type": "application/json"};
+
+        const init = {
+            method: "POST",
+            headers: headers,
+            mode: "cors",
+            body: JSON.stringify(data)
+          };
+  
+        const request = new Request(URL + "/login", init);
+        const r = await fetch(request);
+        const r_data = await r.json();
+  
+        TOKEN = r_data.token;
     };
 
     return ([
         <Nav />,
         <Container maxWidth="sm">
         <Typography variant="h2" component="h2">Login</Typography>
-        <form className={classes.container} onSubmit={handleSubmit()}>
+        <form className={classes.container} onSubmit={(e) => handleSubmit(e)}>
             <TextField
                 id="username"
                 label="Name"
@@ -212,6 +282,143 @@ const Login = () => {
     ]);
 };
 
+const Register = () => {
+    const classes = useStyles();
+    const [values, setValues] = React.useState({
+        username: "",
+        description: "",
+        email: "",
+        password: "",
+    });
+
+    const handleChange = name => event => {
+        setValues({ ...values, [name]: event.target.value });
+    };
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        const data = {
+            "username": values.username,
+            "description": values.description,
+            "email": values.email,
+            "password": values.password
+        };
+
+        const headers = {"Content-Type": "application/json"};
+
+        const init = {
+            method: "POST",
+            headers: headers,
+            mode: "cors",
+            body: JSON.stringify(data)
+          };
+  
+        const request = new Request(URL + "/register", init);
+        const r = await fetch(request);
+        const r_data = await r.json();
+  
+        TOKEN = r_data.token;
+    };
+
+    return ([
+        <Nav />,
+        <Container maxWidth="sm">
+        <Typography variant="h2" component="h2">Register</Typography>
+        <form className={classes.container} onSubmit={(e) => handleSubmit(e)}>
+            <TextField
+                id="username"
+                label="Name"
+                className={classes.textField}
+                onChange={handleChange("username")}
+                margin="normal"
+            />
+            <TextField
+                id="email"
+                label="Email"
+                className={classes.textField}
+                onChange={handleChange("email")}
+                margin="normal"
+            />
+            <TextField
+                id="description"
+                label="Description"
+                className={classes.textField}
+                onChange={handleChange("description")}
+                margin="normal"
+            />
+            <TextField
+                id="password"
+                type="password"
+                label="Password"
+                className={classes.textField}
+                onChange={handleChange("password")}
+                margin="normal"
+            />
+            <Button label="Submit" type="submit" variant="contained" className={classes.button}>
+                Register
+            </Button>
+        </form>
+        </Container>
+    ]);
+};
+
+const SubmitContent = () => {
+    const classes = useStyles();
+    const [values, setValues] = React.useState({
+        content: "",
+    });
+
+    const handleChange = name => event => {
+        setValues({ ...values, [name]: event.target.value });
+    };
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        const data = {
+            "content": values.content,
+        };
+
+        const headers = {
+            "Authorization": TOKEN,
+            "Content-Type": "application/json"
+        };
+
+        const init = {
+            method: "POST",
+            headers: headers,
+            mode: "cors",
+            body: JSON.stringify(data)
+          };
+  
+        const request = new Request(URL + "/content", init);
+        const r = await fetch(request);
+        const r_data = await r.json();
+        
+        console.log(r_data.body);
+    };
+
+    return ([
+        <Nav />,
+        <Container maxWidth="sm">
+        <Typography variant="h2" component="h2">Submit content</Typography>
+        <form className={classes.container} onSubmit={(e) => handleSubmit(e)}>
+            <TextField
+                id="content"
+                label="Content"
+                className={classes.textField}
+                onChange={handleChange("content")}
+                margin="normal"
+            />
+            <Button label="Submit" type="submit" variant="contained" className={classes.button}>
+                Submit
+            </Button>
+        </form>
+        </Container>
+    ]);
+};
+
 const Home = () => {
     const [stack, setStack] = useState([]);
     const classes = useStyles();
@@ -221,26 +428,53 @@ const Home = () => {
     }, []);
 
     async function getStack() {
-        const r = await fetch(URL + "/u/1/s");
-        const stack = await r.json();
-        setStack(stack.data);
+        const headers = new Headers({
+            "Authorization": TOKEN
+        });
+
+        const init = {
+          method: "GET",
+          headers: headers,
+          mode: "cors",
+        };
+
+        const request = new Request(URL + "/stack", init);
+        const r = await fetch(request);
+        const r_data = await r.json();
+
+        setStack(r_data.data);
     }
 
-    const handleChoice = (choice) => event => {
+    async function handleChoice(event, choice) {
         event.preventDefault();
+
         console.log(`User selected: ${choice}`);
-        
-        const r_url = `${URL}/u/1/v`;
 
-        axios.post(`${r_url}/${stack[choice].id}/1`)
-            .then(data => console.log("OK:" + data))
-            .catch(e => console.log("Error: " + e));
-        
         const other = (choice == 0) ? 1 : 0;
+        const vote = {
+            "win": stack[choice]._id.$oid,
+            "los": stack[other]._id.$oid
+        }
 
-        axios.post(`${r_url}/${stack[other].id}/0`)
-            .then(data => console.log("OK:" + data))
-            .catch(e => console.log("Error: " + e));
+        const headers = new Headers({
+            "Authorization": TOKEN,
+            "Content-Type": "application/json"
+        });
+
+        const init = {
+          method: "POST",
+          headers: headers,
+          mode: "cors",
+          body: JSON.stringify(vote)
+        };
+
+        const request = new Request(URL + "/vote", init);
+        const r = await fetch(request);
+        const r_data = await r.json();
+
+        console.log(r_data.message);
+
+        getStack();
     };
 
     return ([
@@ -250,43 +484,30 @@ const Home = () => {
         If u wanna get rich, u better vote bitx!
         </Typography>
         <br></br>
-        <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-            spacing={2}
-        >
-            <Grid item xs>
-                <Paper className={classes.paper} onClick={handleChoice(0)} component={Button} to={"/"}>
-                    <Typography component="p">
-                        <b>{stack[0] && stack[0].content}</b>
-                        <br></br>
-                        by <i>user_{stack[0] && stack[0].user_id}</i> 
-                    </Typography>
-                </Paper> 
-            </Grid>
-            <Grid item xs>
-                <Paper className={classes.paper} onClick={handleChoice(1)} component={Button} to={"/"}>
-                    <Typography component="p">
-                        <b>{stack[1] && stack[1].content}</b> 
-                        <br></br>
-                        by <i>user_{stack[1] && stack[1].user_id}</i> 
-                    </Typography>
-                </Paper> 
-            </Grid>
+        <Grid container direction="row" justify="center" alignItems="center" spacing={2}>
+            {stack.map((s, i) =>
+                <Grid item xs>
+                    <Paper className={classes.paper} onClick={(e) => handleChoice(e, i)} component={Button} to={"/"}>
+                        <Typography component="p">
+                            <b>{s.content.data}</b>
+                        </Typography>
+                    </Paper> 
+                </Grid>
+            )}
         </Grid>
     </Container>
     ]);
 };
 
 ReactDOM.render(
-<Router>
-    <Route exact path="/" component={Home}/>
-    <Route path="/login" component={Login}/>
-    <Route path="/register" component={Login}/>
-    <Route path="/u/:id" component={User}/>
-    <Route path="/ranking" component={Ranking}/>
-</Router>,
-document.getElementById('root')
+    <Router>
+        <Route exact path="/" component={Home}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/register" component={Register}/>
+        <Route path="/user" component={User}/>
+        <Route path="/submit" component={SubmitContent}/>
+        <Route path="/ranking" component={Ranking}/>
+        <Route path="/ranking2" component={Ranking2}/>
+    </Router>,
+    document.getElementById('root')
 );
